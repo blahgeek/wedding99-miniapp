616eca6f21116510cdd9f7d662e9c16be1129a9d
Yikai Zhao <yikai@z1k.dev>
2023-05-27 23:53:43 +0800

    admin: list and order hunt score

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from .models import RsvpResponse, UiConfig, HuntQuestion, HuntScore
 
 
+class HuntScoreAdmin(admin.ModelAdmin):
+    list_display = ('openid', 'name', 'score')
+    ordering = ('-score',)
+
+
 admin.site.register(RsvpResponse)
 admin.site.register(UiConfig)
 admin.site.register(HuntQuestion)
-admin.site.register(HuntScore)
+admin.site.register(HuntScore, HuntScoreAdmin)
