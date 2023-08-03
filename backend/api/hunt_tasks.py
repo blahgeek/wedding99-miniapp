d1b2a99e6bcb810f8c11fd49317293ca562d6f90
Yikai Zhao <yikai@z1k.dev>
2023-08-04 00:42:50 +0800

    update 5 hunt tasks

--- backend/api/hunt_tasks.py
+++ backend/api/hunt_tasks.py
@@ -34,24 +34,69 @@ class HuntTask:
 
 ALL_TASKS = [
     HuntTask(
-        id='q0',
-        name='Q 0',
+        id='q_ouyang',
+        name='谁最帅',
         defaultLocked=True,
         taskDetail=QuestionTaskDetail(
-            questionRichContent='<h1>Question 1</h1>',
-            answers=['A', 'B', 'C', 'D'],
+            questionRichContent='''
+            <h3>以下哪一位是新娘最喜欢的明星？</h3>
+            <img width="100%" src="https://wedding-photo.blahgeek.com/assets/question_ouyang.jpg" />
+            ''',
+            answers=[' A. 欧阳震华', ' B. 陈冠希', ' C. 周杰伦', ' D. 吴彦祖'],
             correctAnswer=0,
-            explanation='Explanation 1',
+            explanation='新娘说明显欧阳震华是最帅的',
+            removeAnswersAfterAd=[1, 3],
+        )
+    ),
+    HuntTask(
+        id='q_whoishe',
+        name='谁最帅2',
+        defaultLocked=True,
+        taskDetail=QuestionTaskDetail(
+            questionRichContent='''
+            <h3>以下哪张是新郎小时候的照片？</h3>
+            ''',
+            answers=[' A', ' B', ' C', ' D'],
+            correctAnswer=1,
+            explanation='',
             removeAnswersAfterAd=[],
+        )
+    ),
+    HuntTask(
+        id='q_coding_0',
+        name='Leetcode0',
+        defaultLocked=True,
+        taskDetail=QuestionTaskDetail(
+            questionRichContent='''
+            <i>请在现场找一位程序员，和Ta搭讪，然后让Ta告诉你这道题的答案。
+            （如果你就是程序员，那很幸运，这是道送分题...）</i>
+            <br />
+            c++中，以下为真的是：
+            ''',
+            answers=['0=0', '1&1', '2-2', '3^3'],
+            correctAnswer=1,
+            explanation='先不管对不对。加微信了吗？',
+            removeAnswersAfterAd=[0],
         ),
     ),
     HuntTask(
-        id='q3',
-        name='合照2',
-        defaultLocked=False,
+        id='q_photo_1',
+        name='合照1',
+        defaultLocked=True,
         taskDetail=PhotoTaskDetail(
-            descriptionRichContent='<h1>Take a photo with two people</h1>',
-            successExplanation='success explanation 2',
+            descriptionRichContent='<h3>现场有一位刚入学的大学生，请找到TA并与TA合照</h3>',
+            successExplanation='',
+            requiredFaceCount=2,
+            requiredUniqueFaceCount=1,
+        ),
+    ),
+    HuntTask(
+        id='q_photo_2',
+        name='合照2',
+        defaultLocked=True,
+        taskDetail=PhotoTaskDetail(
+            descriptionRichContent='<h3>现场有很多位律师，请找到其中一位并与TA合照</h3>',
+            successExplanation='',
             requiredFaceCount=2,
             requiredUniqueFaceCount=1,
         ),
