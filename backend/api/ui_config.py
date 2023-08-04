3ef299c30b714cd86ec5e16642dfdd7be8015dd4
Yikai Zhao <yikai@z1k.dev>
2023-08-04 23:29:19 +0800

    clear server global config

--- backend/api/ui_config.py
+++ backend/api/ui_config.py
@@ -5,13 +5,4 @@ from textwrap import dedent
 
 
 UI_CONFIGS = {
-    'rsvpNoticeHtml': dedent('''
-        <p>朋友小聚，我们不收礼金</p>
-        <p>时间：2023年9月9日晚 18:00</p>
-        <p>地点：罗兰湖餐厅</p>
-    '''),
-    'huntHtml': dedent('''
-    <p>在婚礼现场完成任务，完成最多的人有礼物噢！</p>
-    <p>一些任务需要在现场找到相应二维码扫码才能解锁。</p>
-    ''')
 }
